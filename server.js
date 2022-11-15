91f4f39cc0f696e53279050f14cd2e920f945119
michaelj-github <mike@michaeljmurphy.com>
2022-11-14 20:28:40 -0500

    listen on 0.0.0.0

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const app = require("./app");
 
 const port = +process.env.PORT || 3000;
 
-app.listen(port, () => {
+app.listen(port, "0.0.0.0", () => {
   console.log(
     // `a server is listening for requests on port ${port} at http://localhost:${port}/`
     `a server is listening for requests`
